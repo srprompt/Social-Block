befd1b558c9d357d8011d7632f856d652cc31ab9
Giovanni <75858528+srprompt@users.noreply.github.com>
2023-11-27 13:23:01 -0300

    Update

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const makeRequest = axios.create({
   //baseURL: "http://192.168.15.149:8800/api/",
-  baseURL: "https://coral-app-xcxp2.ondigitalocean.app:8080/api/",
+  baseURL: "https://coral-app-xcxp2.ondigitalocean.app/api/",
   withCredentials: true,
 });
