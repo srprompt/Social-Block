bcc4abdd13c62e25e7923cbbd5509278d9936cc9
Giovanni <75858528+srprompt@users.noreply.github.com>
2023-11-27 10:59:33 -0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://seahorse-app-64ieu.ondigitalocean.app/api/",
+  baseURL: "https://seahorse-app-64ieu.ondigitalocean.app:8800/api/",
   withCredentials: true,
 });
